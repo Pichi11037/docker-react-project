556aff6d8866831743a4089073432d3571e510c9
Andres Pichimata <afpichimata@hotmail.com>
2024-04-28 16:11:58 -0500

    detting up github actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Docker deploying live? OK now deploying live!
+          Docker deploying live? OK now deploying live! Is this still live? ok, it is :)
         </p>
       </div>
     );
